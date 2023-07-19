f1ab29ea48f3e09d8f79c1a6315e6d20f4e370f1
Lucas Rodrigues <lucasrodrigues2606@gmail.com>
2023-07-19 09:00:06 -0300

    att2

--- app/src/main/java/com/example/desafio3teste/MainActivity.java
+++ app/src/main/java/com/example/desafio3teste/MainActivity.java
@@ -15,100 +15,102 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView questionTextView;
-    private RadioGroup optionsRadioGroup;
-    private Button submitButton;
-
-    private List<String> questions;
-    private List<String[]> options;
-    private List<Integer> correctAnswers;
-    private List<Integer> selectedAnswers;
-
-    private int currentQuestion;
+    private TextView textoPergunta;
+    private RadioGroup grupoOpcoes;
+    private Button botaoEnviar;
+    private List<String> perguntas;
+    private List<String[]> opcoes;
+    private List<Integer> respostasCorretas;
+    private List<Integer> respostasSelecionadas;
+    private int perguntaAtual;
+    private TextView textoNumeroQuestao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        questionTextView = findViewById(R.id.questionTextView);
-        optionsRadioGroup = findViewById(R.id.opcoesRadioGroup);
-        submitButton = findViewById(R.id.submitButton);
+        textoNumeroQuestao = findViewById(R.id.textoNumeroQuestao);
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        textoPergunta = findViewById(R.id.questionTextView);
+        grupoOpcoes = findViewById(R.id.opcoesRadioGroup);
+        botaoEnviar = findViewById(R.id.submitButton);
+
+        botaoEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int selectedRadioButtonId = optionsRadioGroup.getCheckedRadioButtonId();
+                int idRadioButtonSelecionado = grupoOpcoes.getCheckedRadioButtonId();
 
-                if (selectedRadioButtonId == -1) {
+                if (idRadioButtonSelecionado == -1) {
                     Toast.makeText(MainActivity.this, "Selecione uma opção", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                RadioButton selectedRadioButton = findViewById(selectedRadioButtonId);
-                int answerIndex = optionsRadioGroup.indexOfChild(selectedRadioButton);
-                selectedAnswers.set(currentQuestion, answerIndex);
+                RadioButton radioButtonSelecionado = findViewById(idRadioButtonSelecionado);
+                int indiceResposta = grupoOpcoes.indexOfChild(radioButtonSelecionado);
+                respostasSelecionadas.set(perguntaAtual, indiceResposta);
 
-                if (currentQuestion < questions.size() - 1) {
-                    currentQuestion++;
-                    showQuestion();
+                if (perguntaAtual < perguntas.size() - 1) {
+                    perguntaAtual++;
+                    mostrarPergunta();
                 } else {
                     showScore();
                 }
             }
         });
 
-        initializeQuiz();
-        showQuestion();
+        inicializarQuiz();
+        mostrarPergunta();
     }
 
-    private void initializeQuiz() {
-        questions = new ArrayList<>();
-        questions.add("O Android é baseado em qual sistema operacional?");
-        questions.add("Por onde a aplicação no Android é iniciada?");
-        questions.add("Qual é a cor do céu em um dia ensolarado?");
+    private void inicializarQuiz() {
+        perguntas = new ArrayList<>();
+        perguntas.add("O Android é baseado em qual sistema operacional?");
+        perguntas.add("Por onde a aplicação no Android é iniciada?");
+        perguntas.add("Para usar uma activity dentro do app é preciso fazer o quê?");
 
-        options = new ArrayList<>();
-        options.add(new String[]{"Linux", "iOS", "Windows", "Java"});
-        options.add(new String[]{"Serviço (Service)", "Receptor de Transmissões (Broadcast Receiver)", "Activity Principal", "Provedor de Conteúdo (Content Provider)"});
-        options.add(new String[]{"Azul", "Verde", "Amarelo", "Vermelho"});
+        opcoes = new ArrayList<>();
+        opcoes.add(new String[]{"Linux", "iOS", "Windows", "Java"});
+        opcoes.add(new String[]{"Serviço (Service)", "Receptor de Transmissões (Broadcast Receiver)", "Activity Principal", "Provedor de Conteúdo (Content Provider)"});
+        opcoes.add(new String[]{"Criar uma classe para a activity", "Declará-la no AndroidManifest.xml", "Conectar a activity ao banco de dados", "Adicionar a activity no build.gradle"});
 
-        correctAnswers = new ArrayList<>();
-        correctAnswers.add(0); // Resposta correta para a primeira pergunta
-        correctAnswers.add(2); // Resposta correta para a segunda pergunta
-        correctAnswers.add(0); // Resposta correta para a terceira pergunta
+        respostasCorretas = new ArrayList<>();
+        respostasCorretas.add(0); // Resposta correta para a primeira pergunta
+        respostasCorretas.add(2); // Resposta correta para a segunda pergunta
+        respostasCorretas.add(1); // Resposta correta para a terceira pergunta
 
-        selectedAnswers = new ArrayList<>();
-        for (int i = 0; i < questions.size(); i++) {
-            selectedAnswers.add(-1); // Valor inicial para as respostas selecionadas (-1 indica nenhuma resposta)
+        respostasSelecionadas = new ArrayList<>();
+        for (int i = 0; i < perguntas.size(); i++) {
+            respostasSelecionadas.add(-1); // Valor inicial para as respostas selecionadas (-1 indica nenhuma resposta)
         }
 
-        currentQuestion = 0;
+        perguntaAtual = 0;
     }
 
-    private void showQuestion() {
-        String question = questions.get(currentQuestion);
-        String[] questionOptions = options.get(currentQuestion);
+    private void mostrarPergunta() {
+        String pergunta = perguntas.get(perguntaAtual);
+        String[] opcoesPergunta = opcoes.get(perguntaAtual);
 
-        questionTextView.setText(question);
+        textoNumeroQuestao.setText("Questão " + (perguntaAtual + 1) + "/" + perguntas.size());
 
-        for (int i = 0; i < optionsRadioGroup.getChildCount(); i++) {
-            RadioButton radioButton = (RadioButton) optionsRadioGroup.getChildAt(i);
-            radioButton.setText(questionOptions[i]);
+        textoPergunta.setText(pergunta);
+
+        for (int i = 0; i < grupoOpcoes.getChildCount(); i++) {
+            RadioButton radioButton = (RadioButton) grupoOpcoes.getChildAt(i);
+            radioButton.setText(opcoesPergunta[i]);
         }
 
-        optionsRadioGroup.clearCheck();
-        if (selectedAnswers.get(currentQuestion) != -1) {
-            RadioButton selectedRadioButton = (RadioButton) optionsRadioGroup.getChildAt(selectedAnswers.get(currentQuestion));
-            selectedRadioButton.setChecked(true);
+        grupoOpcoes.clearCheck();
+        if (respostasSelecionadas.get(perguntaAtual) != -1) {
+            RadioButton radioButtonSelecionado = (RadioButton) grupoOpcoes.getChildAt(respostasSelecionadas.get(perguntaAtual));
+            radioButtonSelecionado.setChecked(true);
         }
     }
 
     private void showScore() {
         int score = 0;
-        for (int i = 0; i < questions.size(); i++) {
-            if (selectedAnswers.get(i) == correctAnswers.get(i)) {
+        for (int i = 0; i < perguntas.size(); i++) {
+            if (respostasSelecionadas.get(i) == respostasCorretas.get(i)) {
                 score++;
             }
         }
